a37c3e7d54df8b9fd7b4d1c6a352b3992c0acbef
55 Tech <luisaslan@gmail.com>
2022-06-29 17:29:39 -0300

    Layout pronto
    Layout pronto

--- M3CameraGps/app/src/main/java/com/luiszimermann/myapplication/MainActivity.kt
+++ M3CameraGps/app/src/main/java/com/luiszimermann/myapplication/MainActivity.kt
@@ -2,10 +2,36 @@ package com.luiszimermann.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		btn_gps.setOnClickListener {
+			updateUserLocation()
+		}
+
+		btn_camera.setOnClickListener {
+			openCamera()
+		}
+
+		btn_send.setOnClickListener {
+			sendImageToAPI()
+		}
+	}
+
+	fun updateUserLocation() {
+		Toast.makeText(this, "Atualizando GPS", Toast.LENGTH_SHORT).show()
+	}
+
+	fun openCamera() {
+		Toast.makeText(this, "Atualizando GPS", Toast.LENGTH_SHORT).show()
+	}
+
+	fun sendImageToAPI() {
+		Toast.makeText(this, "Atualizando GPS", Toast.LENGTH_SHORT).show()
 	}
 }
